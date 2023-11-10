029c9e84330c88eef45fc005dd39ff52716a9d5f
miza531 <miza531@vidalibarraquer.net>
2023-11-10 14:20:31 +0100

    server get focus/HomeInfo/IBEX35-chart.json

--- server/src/controllers/apiConnection.controller.js
+++ server/src/controllers/apiConnection.controller.js
@@ -1,31 +1,74 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
+const urls = require("./../data/urls.json");
 
 const apiConnectionCtrl = {};
-const urlApi = 'https://api.bolsasymercados.es/Market/locales/en/common.json?4687300';
-const hostApi = 'www.bolsasymercados.es';
+//const urlApi = 'https://api.bolsasymercados.es/Market/locales/en/common.json?4687300';
+const urlApiCommonHome = urls.ApiInfo.urlApiCommonHome;
+const urlApiCommonIBEX35_Chart = urls.ApiInfo.urlApiCommonIBEX35_Chart;
+const urlFocusJson = urls.ApiInfo.urlApiCommonFocus;
+const hostApi = "www.bolsasymercados.es";
 
-apiConnectionCtrl.getJsonFile = async (req, res) => {
-    const options = {
-        method: 'GET',
-        url: urlApi,
-        host: hostApi
-    };
+// Get common home info //
+apiConnectionCtrl.getHomeInfoJson = async (req, res) => {
+     const options = {
+          method: "GET",
+          url: urlApiCommonHome,
+          host: hostApi,
+     };
 
-    try {
-
+     try {
         const response = await axios.request(options);
         const data = response.data;
-        fs.writeFileSync('IBEX35.json', JSON.stringify(data, null, 2));
+        fs.writeFileSync("HomeInfo.json", JSON.stringify(data, null, 2));
         res.json(data);
-        console.log(data);
-
-    } catch (error) {
+        let xd = NaN
+        let xd2 = null
+        console.log(xd == xd)
+        //console.log(data);
+     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'ERROR: 500 - An error occurred' });
-    }
-}
+        res.status(500).json({ message: "ERROR: 500 - An error occurred" });
+     }
+};
 
+// Get IBEX35 chart info //
+apiConnectionCtrl.getIbex35ChartJson = async (req, res) => {
+   const options = {
+        method: "GET",
+        url: urlApiCommonIBEX35_Chart,
+        host: hostApi,
+   };
 
+   try {
+      const response = await axios.request(options);
+      const data = response.data;
+      fs.writeFileSync("IBEX35-Chart.json", JSON.stringify(data, null, 2));
+      res.json(data);
+      //console.log(data);
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "ERROR: 500 - An error occurred" });
+   }
+};
+
+apiConnectionCtrl.getFocusJson = async (req, res) => {
+   const options = {
+        method: "GET",
+        url: urlFocusJson,
+        host: hostApi,
+   };
+
+   try {
+      const response = await axios.request(options);
+      const data = response.data;
+      fs.writeFileSync("Focus.json", JSON.stringify(data, null, 2));
+      res.json(data);
+      //console.log(data);
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "ERROR: 500 - An error occurred" });
+   }
+};
 
 module.exports = apiConnectionCtrl;
--- server/src/routes/apiConnection.routes.js
+++ server/src/routes/apiConnection.routes.js
@@ -5,8 +5,10 @@ const apiConnectionCtrl = require('../controllers/apiConnection.controller');
 const router = Router();
 
 
-router.get('/', apiConnectionCtrl.getJsonFile);
-
+router.get('/', apiConnectionCtrl.getHomeInfoJson);
+router.get('/home', apiConnectionCtrl.getHomeInfoJson);
+router.get('/ibex35_chart', apiConnectionCtrl.getIbex35ChartJson);
+router.get('/home/focus', apiConnectionCtrl.getFocusJson);
 
 
 module.exports = router;
