e81c15d8e1884126f8c845ef7eb1213fd8a05b49
miza531 <miza531@vidalibarraquer.net>
2023-11-17 14:23:23 +0100

    All general company details --> File Test_ShareDetailsInfo - WORKING

--- server/src/controllers/apiConnection.controller.js
+++ server/src/controllers/apiConnection.controller.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const fs = require('fs/promises');
 const urls = require("./../data/urls.json");
-const { set } = require("mongoose");
 
 const hostApi = "www.bolsasymercados.es";
 
@@ -15,34 +14,39 @@ const companiesKey = urls.ApiInfo.ShareDetailsInfo_CompanyKey
 // Get test //
 apiConnectionCtrl.getTest = async (req, res) => {
    try {
-
+      let data = [];
       for (const [clave, valor] of Object.entries(companiesKey)) {
+
+
+         await new Promise(resolve => setTimeout(resolve, 6000));
+
+
          const options = {
             method: 'GET',
-            url: urlApiShareDetailsInfo + valor,
-            // host: hostApi, // Este campo no es necesario en axios
+            url: urlApiShareDetailsInfo + valor
          };
 
          console.log(options.url);
 
          try {
             const response = await axios(options);
-            console.log('Waiting 6 seconds...', response.data);
+            console.log('Waiting 6 seconds...');
 
             // Guardar la respuesta en el archivo correspondiente
-            await fs.writeFile(`./src/data/Test_${clave}.json`, JSON.stringify(response.data, null, 2));
-
+            //await fs.writeFile(`./src/data/Test_${clave}.json`, JSON.stringify(response.data, null, 2));
+            await data.push(response.data);
+            
             // Esperar 6 segundos antes de la siguiente iteración
-            await new Promise(resolve => setTimeout(resolve, 6000));
          } catch (error) {
-            console.error('Error fetching data for', clave, error);
+            console.error('Error fetching data for');
+            
          }
       }
+      fs.writeFile(`./src/data/Test_ShareDetailsInfo.json`, JSON.stringify(data, null, 2));
 
       res.json({ message: 'Test completed' });
    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'ERROR: 500 - An error occurred' });
+      // console.error(error);
    }
 };
 
