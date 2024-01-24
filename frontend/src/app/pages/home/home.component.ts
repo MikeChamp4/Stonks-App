395e91a9305a3a218a1f774d7a89aca0a967f81d
miza531 <miza531@vidalibarraquer.net>
2024-01-24 09:37:01 +0100

    table company values in process

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { InputSwitchModule } from 'primeng/inputswitch';
-
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-home-page',
@@ -9,27 +9,48 @@ import { InputSwitchModule } from 'primeng/inputswitch';
   styleUrls: ['./home.component.scss']
 })
 export class HomePageComponent {
-  themeSelection: boolean = false;
-  checked: boolean = false;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor(@Inject(DOCUMENT) private document: Document){
-    let theme = localStorage.getItem('theme');
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
-    if(theme){
-      this.themeSelection = theme == 'dark' ? true : false;
-      this.changeThemeBasedOnBoolean(this.themeSelection);
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
     }
   }
-
-  changeThemeBasedOnBoolean(state: boolean) {
-    let theme = state ? 'dark' : 'light';
-    window.localStorage.setItem('theme', theme);
-    let themeLink = this.document.getElementById ('app-theme') as HTMLLinkElement;
-    themeLink.href = `lara-${theme}-blue.css`;
-  }
-
-  changeTheme(event: Event) {
-    let state = (event.target as HTMLInputElement).checked;
-    this.changeThemeBasedOnBoolean(state);
-  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
